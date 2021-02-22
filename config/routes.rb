c949a7d4eab8b124f88fd42d8eb85ff29e113808
Parker Jones <parker.harold.jones@gmail.com>
2021-02-22 08:13:25 -0700

    Controllers made

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    get 'items/skip-routes'
+  end
+  namespace :api do
+    get 'menus/skip-routes'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
