3bd8009308f4a30a25f9a3fe5d687b38d0a457a2
Parker Jones <parker.harold.jones@gmail.com>
2021-02-22 08:43:06 -0700

    created cruds

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,4 +1,52 @@
 class Api::ItemsController < ApplicationController
-  def skip-routes
-  end
+  before_action :set_menu
+  before_action :set_item, only: [:show, :update, :destroy]
+
+  def index 
+    render json: @menu.items
+  end 
+
+  def show 
+    render json: @item
+  end 
+
+  def create 
+    # item = Item.new
+    @item = @menu.items.new(item_params)
+    if @item.save
+      render json: @item
+    else 
+      render json: { errors: @item.errors }, status: :unprocessable_entity
+    end 
+  end 
+
+  def update 
+    if @item.update(item_params)
+      render json: @item
+    end 
+
+    else
+     render json: { errors: @item.errors }, status: :unprocessable_entity 
+    end 
+
+  end 
+
+  def destroy 
+    @item.destroy
+    render json: { message: 'item deleted'}
+  end 
+
+  private 
+  def set_menu
+  @menu = Menu.find(params[:menu_id])  
+  end   
+
+  def set_item
+    @item = @menu.items.find(params[:id])
+  end 
+
+  def item_params
+    params.require(:item).permit(:item_name, :price)
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  
   namespace :api do
-    get 'items/skip-routes'
+   resources :menus do
+    resources :items 
+   end 
   end
-  namespace :api do
-    get 'menus/skip-routes'
-  end
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
 end
