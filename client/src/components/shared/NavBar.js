45d9d2d515872a4ed1d841d2510730ce8b41a0ef
Parker Jones <parker.harold.jones@gmail.com>
2021-02-23 08:58:16 -0700

    Made NavBar

---
+++ client/src/components/shared/NavBar.js
@@ -0,0 +1,29 @@
+import { Menu } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => (
+    <Menu>
+       
+        <Link to='/'>
+            <Menu.Item>
+                Home
+            </Menu.Item>
+        </Link>
+
+        <Link to='/about'>
+            <Menu.Item>
+                About
+            </Menu.Item>
+        </Link>
+
+        <Link to='/menus'>
+            <Menu.Item>
+                Menu
+            </Menu.Item>
+        </Link>
+
+
+    </Menu>
+)
+
+export default Navbar; 
