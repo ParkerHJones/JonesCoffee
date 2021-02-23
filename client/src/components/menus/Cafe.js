7f838dcd8093f8340bec431e741d5794ffce0051
Parker Jones <parker.harold.jones@gmail.com>
2021-02-23 09:21:06 -0700

    Created Crud actions in Cafe

--- client/src/components/menus/Cafe.js
+++ client/src/components/menus/Cafe.js
@@ -1,16 +1,32 @@
 import { Component } from 'react';
 import axios from 'axios'; 
+import MenuForm from './MenuForm';
 
 class Cafe extends Component {
     state = { menus: [] }
 
     componentDidMount () {
         // grabs menus from db //sets to state 
+        axios.get('/api/menus')
+         .then ( res => {
+             this.setState({ menus: res.data })
+         })
+         .catch( err => {
+             console.log(err);
+         })
     }
 
     addMenu = () => {
         //add to the db
+        axios.get('/api/menus',  { menu }) // needs to pass menu to know what to create
         //ad to the state
+        .then(res => {
+            const { menus } = this.state;
+            this.setState({ menus: [...menus, res.data]}) 
+        })
+        .catch( err => {
+            console.log(err);
+        })
     }
 
     updateMenu = (id, menu) => {
@@ -26,6 +42,7 @@ class Cafe extends Component {
         return (
             <>
             <h1> The Cafe </h1>
+            <MenuForm addMenu={this.addMenu} />
             </>
         )
     }
